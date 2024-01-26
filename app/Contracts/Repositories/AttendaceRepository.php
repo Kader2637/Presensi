a5b37dd479be093eebaa7249f72943e631c13ff2
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-01-26 10:23:34 +0700

    attendance

---
+++ app/Contracts/Repositories/AttendaceRepository.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Contracts\Repositories;
+
+use App\Contracts\Interfaces\AttendanceInterface;
+use App\Contracts\Repositories\BaseRepository;
+use App\Models\attendance;
+
+class AttendaceRepository extends BaseRepository implements AttendanceInterface
+{
+    public function __construct(attendance $attendance)
+    {
+        $this->model = $attendance ;
+    }
+        
+    public function get(): mixed
+    {
+
+    }
+
+    public function store(array $data): mixed
+    {
+
+    }
+}
+
