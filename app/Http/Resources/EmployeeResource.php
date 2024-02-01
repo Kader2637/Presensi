9e6a888ff2404eaf7cb1392ceefe3cb2642488eb
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-02-01 15:30:42 +0700

    Update EmployeeResource.php

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -15,6 +15,7 @@ class EmployeeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->user->name,
             'email' => $this->user->email,
             'id_number' => $this->nik,
