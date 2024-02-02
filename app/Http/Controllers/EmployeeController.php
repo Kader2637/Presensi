2dcdc2eac4660d9d4b0b092f2169d965f9e7f49c
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-02-02 11:11:29 +0700

    Create EmployeeController.php

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\Interfaces\EmployeeInterface;
+use App\Contracts\Interfaces\UserInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\EmployeeRequest;
+use App\Http\Resources\EmployeeResource;
+use App\Models\Employee;
+use App\Services\EmployeeService;
+use Illuminate\Http\JsonResponse;
+
+class EmployeeController extends Controller
+{
+    private EmployeeInterface $employee;
+    private EmployeeService $service;
+    private UserInterface $user;
+    public function __construct(EmployeeInterface $employee, UserInterface $userInterface, EmployeeService $employeeService)
+    {
+        $this->service = $employeeService;
+        $this->user = $userInterface;
+        $this->employee = $employee;
+    }
+
+    public function index(): JsonResponse
+    {
+        return response()->json(['result' => EmployeeResource::collection($this->employee->get())], 200);
+    }
+
+    public function store(EmployeeRequest $request): JsonResponse
+    {
+        $data = $this->service->store($request);
+        $data['user_id'] = $this->user->store($data);
+        $this->employee->store($data);
+        return response()->json(['message' => 'Berhasil menambah pengguna'], 200);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $employee
+     * @return JsonResponse
+     */
+    public function show(Employee $employee): JsonResponse
+    {
+        return response()->json(['result' => EmployeeResource::make($employee)], 200);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $employee
+     * @param  mixed $request
+     * @return JsonResponse
+     */
+    public function update(Employee $employee, EmployeeRequest $request): JsonResponse
+    {
+        $data = $this->service->update($request, $employee);
+        $this->employee->update($employee->id, $data);
+        return response()->json(['message' => 'Berhasil mengubah pengguna'], 200);
+    }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $employee
+     * @return JsonResponse
+     */
+    public function destroy(Employee $employee): JsonResponse
+    {
+        if ($employee->photo != null) $this->service->remove($employee->photo);
+        $this->user->delete($employee->user->id);
+        return response()->json(['message' => 'Berhasil menghapus pengguna'], 200);
+    }
+}
