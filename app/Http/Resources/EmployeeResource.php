78e527f9a2c258ad8a1cfe18ba8f93bc7df7947b
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-02-29 14:37:48 +0700

    update

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -27,6 +27,7 @@ class EmployeeResource extends JsonResource
             'rfid' => $this->rfid,
             'address' => $this->address,
             'date_of_birth' => $this->date_of_birth,
+            'faces' => FaceResource::collection($this->faces),
         ];
     }
 }
