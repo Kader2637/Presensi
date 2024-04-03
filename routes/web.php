769caf94d09019f99a150950148f3452d87c0277
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-04-03 13:51:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,30 +23,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::get('dashboard', [HomeController::class ,'dashboard']);
-
-Route::get('position', [PositionController::class, 'index']);
-
-Route::post('position', [PositionController::class, 'store']);
-Route::put('position/{position}', [PositionController::class, 'update']);
-Route::delete('delete-position/{position}', [PositionController::class, 'destroy']);
-
-Route::get('attendance', [AttendanceController::class, 'getAttendance'])->name('attendance.index');
-
-Route::get('employee', [EmployeeController::class, 'index']);
-Route::post('employe' , [EmployeeController::class , 'store'])->name('employe.store');
-Route::put('employee/{employee}', [EmployeeController::class, 'update']);
-Route::delete('delete-employee/{employee}', [EmployeeController::class, 'destroy']);
-Route::get('division', [DivisionController::class, 'index']);
-Route::post('division', [DivisionController::class, 'store']);
-Route::put('division/{division}', [DivisionController::class, 'update']);
-Route::delete('delete-division/{division}', [DivisionController::class, 'destroy']);
 Auth::routes();
-Route::get('absensi' , [AttendanceController::class , 'getAttendance']);
-Route::get('data-absensi/export/excel', [AttendanceController::class, 'export_excel'])->name('list.attendance.admin.export.excel');
-Route::get('face' , [FaceController::class , 'index']);
-Route::get('face/detail/{id}' , [FaceController::class , 'show']);
-Route::post('face/create', [FaceController::class, 'store']);
-Route::put('face/update/{employee}', [FaceController::class, 'update'])->name('face.update');
-Route::delete('face/delete/{employee}', [FaceController::class, 'destroy'])->name('face.destroy');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', [HomeController::class ,'dashboard']);
+    Route::get('attendance', [AttendanceController::class, 'getAttendance'])->name('attendance.index');
+    Route::get('employee', [EmployeeController::class, 'index']);
+    Route::post('employe' , [EmployeeController::class , 'store'])->name('employe.store');
+    Route::put('employee/{employee}', [EmployeeController::class, 'update']);
+    Route::delete('delete-employee/{employee}', [EmployeeController::class, 'destroy']);
+    Route::get('absensi' , [AttendanceController::class , 'getAttendance']);
+    Route::get('data-absensi/export/excel', [AttendanceController::class, 'export_excel'])->name('list.attendance.admin.export.excel');
+    Route::get('face' , [FaceController::class , 'index']);
+    Route::get('face/detail/{id}' , [FaceController::class , 'show']);
+    Route::post('face/create', [FaceController::class, 'store']);
+    Route::put('face/update/{employee}', [FaceController::class, 'update'])->name('face.update');
+    Route::delete('face/delete/{employee}', [FaceController::class, 'destroy'])->name('face.destroy');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
