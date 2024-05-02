234a815d357f122acf51a75e2ae0c72ab91f8975
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-02 09:03:42 +0700

    Update EmployeeFaceResource.php

--- app/Http/Resources/EmployeeFaceResource.php
+++ app/Http/Resources/EmployeeFaceResource.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
@@ -10,13 +9,15 @@ class EmployeeFaceResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
+     * @param Request $request
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray($request): array
     {
         return [
             'rfid' => $this->rfid,
-            'faces' => FaceResource::collection($this->faces),
+            'md5' => md5($this->faces),
+            'faces' => EmployeeFaceResource::collection($this->faces),
         ];
     }
 }
